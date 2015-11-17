f9a17eb7dd37c6faeecd32b6b7e18fe9db5c5117
3guy <friends@aliyun.com>
2015-11-17 09:35:09 +0800

    Placeorder
    placeorder

---
+++ ThrFrd.GameTrans/ThrFrd.GameTrans.Admin.Biz/PlaceOrderBiz.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThrFrd.GameTrans.Infrastructure.Entities;
+using ThrFrd.GameTrans.Infrastructure.Entities.EFContext;
+using ThrFrd.GameTrans.Infrastructure.Entities.Entity;
+using ThrFrd.GameTrans.Tool;
+
+namespace ThrFrd.GameTrans.Admin.Biz
+{
+   public class PlaceOrderBiz
+    {
+       public static ItemListBox<OrderBase> GetPlaceOrderListByPage(int page, int pageSize, string key, int status, string starttime, string endtime, ref int count)
+       {
+
+            try
+            {
+                using (Context ctx = new Context())
+                {
+                    string userName = System.Web.HttpContext.Current.User.Identity.Name;
+                    var expression = from o in ctx.OrderBase  where o.CreaterUser == userName
+                                     join d in ctx.OrderDetail on o.ID equals d.OrderID
+                                     join p in ctx.Product on o.ID equals p.OrderID
+                                     join ga in ctx.Game on p.GameID equals ga.id into gm
+                                     from g in gm.DefaultIfEmpty()
+                                     select new { o, d, p, g };
+
+                    if (!string.IsNullOrEmpty(key))
+                    {
+                        expression = expression.Where(a => a.o.CreaterUser.Contains(key) || a.o.Source.Contains(key) || a.p.Name.Contains(key) 
+                            || a.p.Description.Contains(key) || (a.g != null && a.g.name.Contains(key)));
+                    }
+                    if (status != -100)
+                    {
+                        expression = expression.Where(a => a.o.State == status);
+                    }
+                    if (!string.IsNullOrEmpty(starttime))
+                    {
+                        DateTime startTime = DateTime.Parse(starttime + " 00:00:00");
+                        expression = expression.Where(a => a.o.CreateTime >= startTime);
+                    }
+                    if (!string.IsNullOrEmpty(endtime))
+                    {
+                        DateTime endTime = DateTime.Parse(endtime + " 23:59:59");
+                        expression = expression.Where(a => a.o.CreateTime < endTime);
+                    }
+
+                    List<OrderBase> orderList = expression
+                        .ToList()
+                        .GroupBy(a => new { a.o })
+                        .Select(a => new OrderBase().Set(a.FirstOrDefault().o))
+                        .Skip(page * pageSize)
+                        .Take(pageSize)
+                        .ToList();
+
+                    return new ItemListBox<OrderBase>(orderList).BuildPage(count, page, pageSize, new PageParameter() { Style = "admin" });
+
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Write(CommonLogger.Application, ex);
+            }
+            return null;
+        }
+       
+
+    }
+}
--- ThrFrd.GameTrans/ThrFrd.GameTrans.Admin.Web/Controllers/PlaceOrderController.cs
+++ ThrFrd.GameTrans/ThrFrd.GameTrans.Admin.Web/Controllers/PlaceOrderController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThrFrd.GameTrans.Admin.Biz;
 using ThrFrd.GameTrans.Admin.WebLogic;
 
 namespace ThrFrd.GameTrans.Admin.Web.Controllers
@@ -15,8 +16,12 @@ namespace ThrFrd.GameTrans.Admin.Web.Controllers
         //
         // GET: /PlaceOrder/
 
-        public ActionResult Index()
+        public ActionResult Index(int page = 0, int pageSize = 10, string key = "", int status = -100, string starttime = "", string endtime = "")
         {
+            int count = 0;
+            ViewBag.orderlist = PlaceOrderBiz.GetPlaceOrderListByPage(page, pageSize, key, status, starttime, endtime, ref count);
+            ViewBag.strquery = string.Format("{0},{1},{2},{3}", key, status, starttime, endtime);
+
             return View();
         }
 
